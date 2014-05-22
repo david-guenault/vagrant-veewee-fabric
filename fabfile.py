5781d262d0a12efe17ab1ff464511a1fc6a026b2
David GUENAULT <david.guenault@gmail.com>
2014-05-22 07:54:04 +0200

    splited installation phases

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 from fabric.api import run
 from fabric.api import local
@@ -12,47 +14,53 @@ from fabric.contrib import files
 env.docker = ['ubuntu']
 
 env.roledefs = {
-    "local" : ["root@localhost"]
+    "local" : ["localhost"]
 }
 
 """ vagrant related """
 
 @roles("local")
-def install_vagrant(clean=False,user=False):
+def install_rvm4user(user=False,password=False):
 
-    # if not user:
-    #     print "you must provide your user name"
-    #     sys.exit(2)
+    if not user or not password:
+        print "user and password are mandatory"
+        print "fab install_rvm4user:user=username,password=*******"
+        sys.exit(2)
 
-    # run("apt-get update")
+    env.user = root
+    run("apt-get update")
+    run("apt-get -y install curl git-core  build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion")
 
-    # if clean == "true":
-    #     run("apt-get -y -f remove virtualbox* vagrant")
-    #     run("apt-get -y autoremove")
 
+    HOME = "/home/%s" % user
+    env.user = user
+    env.password = password
+
+    with cd(HOME):
+        run("curl -sS https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer > %s/rvm.sh" % HOME,shell=True)
+        run("chmod +x %s/rvm.sh " % HOME)
+        run("%s/rvm.sh" % HOME)
+        files.append("%s/.profile" % HOME,"source %s/.rvm/scripts/rvm" % HOME)
+        run("source %s/.profile && rvm install 1.9.2" % HOME)
+        files.append("%s/profile" % HOME,"rvm use 1.9.2 --default")    
+        run("rm -f %s/rvm.sh" % HOME)
+
+
+@roles("local")
+def install_vagrant(clean=False):
     # """ virtualbox """
-    # run("apt-get -y install virtualbox git-core")        
-    # run("usermod -a -G vboxusers %s" % user)
+    env.user = "root"
+    run("apt-get -y install virtualbox")        
 
     # """ vagrant """
-    # run("cd /tmp && wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_x86_64.deb")
-    # with settings(warn_only=True):
-    #     run("cd /tmp && dpkg -i vagrant_1.6.2_x86_64.deb")
-    #     run("apt-get -y install -f")
+    run("cd /tmp && wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_x86_64.deb")
+    with settings(warn_only=True):
+         run("cd /tmp && dpkg -i vagrant_1.6.2_x86_64.deb")
+         run("apt-get -y install -f")
 
     # """ prerequisites """
     # run("apt-get -y install curl libxslt1-dev libxml2-dev build-essential")
 
-    """ rvm """
-    with cd("/home/%s" % user):
-        #run("apt-get -y install curl git-core  build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion")
-        sudo("curl -sS https://get.rvm.io > /home/%s/rvm.sh" % user,user=user,shell=True)
-        sudo("chmod +x /home/%s/rvm.sh " % user)
-        sudo("/home/%s/rvm.sh" % user)
-
-    #     files.append("/home/%s/.profile" % user,"source /usr/local/rvm/scripts/rvm")
-    #     sudo("source /etc/profile && rvm install 1.9.2")
-    #     files.append("/etc/profile","rvm use 1.9.2 --default")    
 
     # """ install veewee """
     # with cd("/home/%s" % user):
@@ -62,6 +70,14 @@ def install_vagrant(clean=False,user=False):
     #     sudo("rvm use 1.9.2@veewee --create",user=user)
     #     sudo("gem install bundler",user=user)
     #     sudo("bundle install",user=user)
+
+@roles("local")
+def enable_virtualbox4user(user=False):
+    if not user:
+        print "user is mandatory"
+        sys.exit(2)
+
+    run("usermod -a -G vboxusers %s" % user)
 
 @roles("local")
 def buildbox(name=False,template=False,user=False):
